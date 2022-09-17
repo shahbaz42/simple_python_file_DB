b6c76281789a1bcb3a061aa592ca0e7e270c72d0
Shahbaz <shahbaz_ali@outlook.in>
2022-09-18 05:00:25 +0530

    add code for displaying transactions

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import traceback
 account_schema = ["account_no", "name", "address", "phone_number", "PAN", "account_type", "balance"]
 
 #Schema of Ledger
-ledger_schema = ['account_1', 'account_2', 'amount', "D/C"]
+ledger_schema = ['account_1', 'account_2', 'amount']
 
 # code for opening/creating csv files
 if not os.path.exists("./Accounts.csv") and not os.path.exists("./Ledger.csv"):
@@ -39,11 +39,19 @@ def create_account(account_no, name, address, phone_number, PAN, account_type, b
     })
     f_ob.close()
 
-def display_transaction_history():
+def display_transaction_history(account_no):
     """
         Function for displaying transaction history
     """
-    pass
+    f_obj = open('Ledger.csv', 'r')
+    f_reader = csv.DictReader(f_obj)
+    print("\n")
+    for record in f_reader:
+        if record['account_1'] == account_no :
+            print("Debited : ", record)
+        elif record['account_2'] == account_no :
+            print("Credited : ", record)
+    f_obj.close()
 
 
 def transaction(credit_account, debit_account, amount):
@@ -65,7 +73,7 @@ def transaction(credit_account, debit_account, amount):
 
     try :
         for s_record in f_reader_1:
-            if s_record['account_no'] == debit_account and int(s_record['balance']) >= amount:
+            if s_record['account_no'] == debit_account and int(s_record['balance']) >= int(amount):
                 for r_record in f_reader_2:
                     if r_record['account_no'] == credit_account:
 
@@ -79,7 +87,6 @@ def transaction(credit_account, debit_account, amount):
                             'account_1' : debit_account,
                             'account_2' : credit_account,
                             'amount' : amount,
-                            'D/C' : 'D'
                         })
                         success = 1
                         break
@@ -115,14 +122,52 @@ def transaction(credit_account, debit_account, amount):
     t1 = time.time()
     print("Time taken for transaction : ", (t1-t0)*1000 , "ms")
     
-
-
 def main():
     """
         main function starts from here.
     """
 
+    end = 1
+    print("===================================================================================")
+    print("\nSchema of Accounts table :\n", account_schema)
+    while end == 1:
+        choice = int(input("\nFor adding a record to Account table Press 1\n" +
+                           "For displaying all Transaction details Press 2\n" +
+                           "For making a Fund Transfer Press 3\n"))
+
+        if choice == 1:
+            account_no = input("Enter Account number of the account holder : ")
+            name = input("Enter Name of the account holder : ")
+            address = input("Enter Address of the account holder : ")
+            phone_no = input("Enter Phone_no of the account holder : ")
+            pan = input("Enter PAN of the account holder : ")
+            type = input("Enter A/C type : ")
+            balance = input("Enter initial deposit amount ")
+
+            create_account(account_no,name,address,phone_no,pan,type,balance)
+            print('\nAccount added successfully')
+
+        elif choice == 2:
+            account = input('Enter your account number : ')
+            display_transaction_history(account)
+
+        elif choice == 3:
+            debit_account = input('\nEnter your Account number : ')
+            credit_account = input('\nEnter the Payee Account number : ')
+            amount = input('\nEnter the amount to transfer : ')
+            transaction(credit_account, debit_account, amount)
+
+        else:
+            print("\nIllegal input !!!")
+
+        end = int(input("\nDo you want to continue ?, If Yes press 1 else press 0\n==>"))
+        if end != 0 and end != 1:
+            print("\nIllegal input !!!")
+            exit()
+
 # test create account
 # create_account("1234", "Shahbaz", "Ballia", "7398358012", "1111111", "Saving", 10000)
 # create_account("4321", "proxymoron", "Ballia", "7398358011", "222222", "Saving", 10000)
-transaction("12345","54321",20)
+# transaction("12345","54321",20)
+
+main()
