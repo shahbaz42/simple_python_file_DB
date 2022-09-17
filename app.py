cbee1fc6911396953339fceb35b4ad2204e86dd3
Shahbaz <shahbaz_ali@outlook.in>
2022-09-18 00:05:21 +0530

    add code for creating DB files

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
+import csv
+import os.path
+from os import path
+
 # Schema of Account table
-schema_account = []
+account_schema = ["account_no", "name", "address", "phone_number", "PAN", "account_type", "balance"]
 
 #Schema of Ledger
-schema_ledger = []
-
+ledger_schema = ['account1', 'account_2', 'amount', "D/C"]
 
 # code for opening/creating csv files
-
-
+if not os.path.exists("./Accounts.csv") and not os.path.exists("./Ledger.csv"):
+    # if DB files do not exist then create new empty files
+    f_ob = open('Accounts.csv', 'a+')
+    f_wr = csv.DictWriter(f_ob, fieldnames=account_schema )
+    f_wr.writeheader()
+    f_ob.close()
+    f_ob = open('./Ledger.csv', 'a+')
+    f_wr = csv.DictWriter(f_ob, fieldnames=ledger_schema)
+    f_wr.writeheader()
+    f_ob.close()
+    
 def create_account():
     """
         Function for creating a new account
