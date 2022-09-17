3a26f72f74073c75e0d232763ae0778306a7bae7
Shahbaz <shahbaz_ali@outlook.in>
2022-09-17 13:47:20 +0530

    structure the program

---
+++ app.py
@@ -0,0 +1,34 @@
+# Schema of Account table
+schema_account = []
+
+#Schema of Ledger
+schema_ledger = []
+
+
+# code for opening/creating csv files
+
+
+def create_account():
+    """
+        Function for creating a new account
+    """
+    pass
+
+def display_transaction_history():
+    """
+        Function for displaying transaction history
+    """
+    pass
+
+def transaction():
+    """
+        Function for making transactions
+    """
+    pass
+
+def main():
+    """
+        main function starts from here.
+    """
+
+main()
